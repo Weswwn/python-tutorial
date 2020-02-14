5c52e211367431d2ed3769017aec4f4d7e7a4b14
Wes <wesley.qb.wen@gmail.com>
2020-02-13 17:56:45 -0800

    Skeleton polling app complete. Last commit before utilizing python's Generic Views system

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
-from django.http import Http404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404
-from .models import Question
 from django.shortcuts import render
+from django.urls import reverse
+from .models import Question
 
 # Create your views here.
 
@@ -16,12 +15,23 @@ def index(request):
 
 def detail(request, question_id):
   question = get_object_or_404(Question, pk=question_id)
-  return render(request, 'polls/details.html', {question: question})
+  return render(request, 'polls/details.html', {'question': question})
 
 def results(request, question_id):
-  response = 'You\'re looking at results of question %s'
-  return HttpResponse(response % question_id)
+  question = get_object_or_404(Question, pk=question_id)
+  return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-  return HttpResponse('You\'re voting on question %s.' % question_id)
+  question = get_object_or_404(Question, pk=question_id)
+  try:
+    selected_choice = question.choice_set.get(pk=request.POST['choice'])
+  except (KeyError, Choice.DoesNotExist):
+    return render(request, 'polls/details.html', {
+      'question': question,
+      'error_message': 'You didn\'t select a valid choice.',
+    })
+  else:
+    selected_choice.votes += 1
+    selected_choice.save()
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
